d0215e5e0a4052bbe05c2946940c9615bd65dcb0
Masuda Kugo <qusyoku9567@icloud.com>
2020-10-09 11:35:43 +0900

    課題提出

---
+++ 2-11/index.php
@@ -0,0 +1,22 @@
+<form action="result.php" method="post">
+  名前：<input type="text" name="my_name" />
+  <br>
+  メールアドレス : <input type="text" name="email" />
+  <br>
+  パスワード：<input type="password" name="password" />
+  <br>
+  <input type="submit" value="送信" />
+</form>
+
+
+<?php 
+// 仕様書・設計書
+// お客さまと一緒に作り上げるのが仕様書 作る人たちだけで作り上げるのが設計書
+
+// Git
+// 分散バージョン管理システム
+
+// マージツール
+// 複数のファイルやデータ、プログラムなどを、決められたルールに従って一つに統合するツール
+
+?>
