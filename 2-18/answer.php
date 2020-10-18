cf4d2060737a1b8e4701d892097f208ce3b9e45f
Masuda Kugo <qusyoku9567@icloud.com>
2020-10-18 13:56:50 +0900

    2-18

---
+++ 2-18/answer.php
@@ -0,0 +1,58 @@
+<?php 
+//[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
+$name = $_POST['name'];
+$port_answer = $_POST['port'];
+$word_answer = $_POST['word'];
+$mysql_answer = $_POST['mysql'];
+$answers = $_POST['answer'];
+// var_dump($answers);
+
+//選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
+// function getAnswer($answer){
+//   if($answer === $answers[0]){
+//       echo '正解';
+//   }else{
+//       echo '不正解';
+//   }
+// }
+// getAnswer($port_answer);
+
+    ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+  <link rel="stylesheet" href="text.css">
+</head>
+<body>
+    <div class="wrapper">
+    <link rel="stylesheet" href="text.css">
+    <p><!--POST通信で送られてきた名前を表示--><?php echo $name; ?>さんの結果は・・・？</p>
+    <p>①の答え</p>
+    <p><?php if ($port_answer == $answers[0]){
+    echo  '正解です';
+}else{
+    echo '不正解です';
+} ?></p>
+    <!--作成した関数を呼び出して結果を表示-->
+    <p>②の答え</p>
+    <p><?php if ($word_answer == $answers[1]){
+    echo  '正解です';
+}else{
+    echo '不正解です';
+} ?></p>
+    <!--作成した関数を呼び出して結果を表示-->
+    <p>③の答え</p>
+    <!--作成した関数を呼び出して結果を表示-->
+    <p><?php if ($mysql_answer == $answers[2]){
+    echo  '正解です';
+}else{
+    echo '不正解です';
+} ?></p>
+   </div>
+</body>
+</html>
---
+++ 2-18/question.php
@@ -0,0 +1,53 @@
+<?php
+//POST送信で送られてきた名前を受け取って変数を作成
+$name = $_POST['name'];
+//①画像を参考に問題文の選択肢の配列を作成してください。
+$ports = [80,22,20,21];
+$words = ['PHP','Pyhon','JAVA','HTML'];
+$mysqls = ['join','select','insert','update'];
+//② ①で作成した、配列から正解の選択肢の変数を作成してください
+$answer = [$ports[0],$words[0],$mysqls[0]];
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+  <link rel="stylesheet" href="text.css">
+  <body>
+      <div class="wrapper">
+        <p>お疲れ様です<!--POST通信で送られてきた名前を出力--><?php echo $name; ?>さん</p>
+        <!--フォームの作成 通信はPOST通信で-->
+        <form action="answer.php" method="post">
+            <h2>①ネットワークのポート番号は何番？</h2>
+            <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+            <?php foreach ($ports as $value){?>
+                <input type="radio" name="port" value="<?php echo $value; ?>"><?php echo $value; ?>
+                <?php } ?>
+                <h2>②Webページを作成するための言語は？</h2>
+                <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+                <?php foreach ($words as $value){?>
+                    <input type="radio" name="word" value="<?php echo $value; ?>"><?php echo $value; ?>
+                    <?php } ?>
+                    <h2>③MySQLで情報を取得するためのコマンドは？</h2>
+                    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+                    <?php foreach ($mysqls as $value){?>
+                        <input type="radio" name="mysql" value="<?php echo $value; ?>"><?php echo $value; ?>
+                        <?php } ?>
+                        <input type="hidden" name="answer[]" value="<?php echo $answer[0] ?>">
+                        <input type="hidden" name="answer[]" value="<?php echo $answer[1] ?>">
+                        <input type="hidden" name="answer[]" value="<?php echo $answer[2] ?>">
+                        <input type="hidden" name="name" value="<?php echo $name ?>">
+                        
+                        
+
+                        <br>
+                        <input type="submit" value="回答する" />
+                    
+                    </form>
+                    </div>
+                <!--問題の正解の変数と名前の変数を[answer.php]に送る-->
+      </body>
+            </html>
