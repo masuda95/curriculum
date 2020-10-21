16eeccb5503179afb48bcc3eea943032363976b0
Masuda Kugo <qusyoku9567@icloud.com>
2020-10-21 14:17:31 +0900

    Update answer.php

--- 2-18/answer.php
+++ 2-18/answer.php
@@ -6,6 +6,18 @@ $word_answer = $_POST['word'];
 $mysql_answer = $_POST['mysql'];
 $answers = $_POST['answer'];
 
+// var_dump($answers);
+
+//選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
+function getAnswer($answer,$answers){
+  if($answer === $answers){
+      echo '正解';
+  }else{
+      echo '不正解';
+  }
+}
+
+
     ?>
 
 <!DOCTYPE html>
@@ -22,26 +34,14 @@ $answers = $_POST['answer'];
     <link rel="stylesheet" href="text.css">
     <p><!--POST通信で送られてきた名前を表示--><?php echo $name; ?>さんの結果は・・・？</p>
     <p>①の答え</p>
-    <p><?php if ($port_answer == $answers[0]){
-    echo  '正解です';
-}else{
-    echo '不正解です';
-} ?></p>
+    <p><?php echo getAnswer($port_answer,$answers[0]);?></p>
     <!--作成した関数を呼び出して結果を表示-->
     <p>②の答え</p>
-    <p><?php if ($word_answer == $answers[1]){
-    echo  '正解です';
-}else{
-    echo '不正解です';
-} ?></p>
+    <p><?php echo getAnswer($word_answer,$answers[1]);?></p>
     <!--作成した関数を呼び出して結果を表示-->
     <p>③の答え</p>
     <!--作成した関数を呼び出して結果を表示-->
-    <p><?php if ($mysql_answer == $answers[2]){
-    echo  '正解です';
-}else{
-    echo '不正解です';
-} ?></p>
+    <p><?php echo getAnswer($mysql_answer,$answers[2]);?></p>
    </div>
 </body>
 </html>
